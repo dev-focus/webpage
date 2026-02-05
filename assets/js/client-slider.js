a57a3cb9261b83854c64ff72967a5b3e826ed0b1
simxnherrera <simxnherrera@gmail.com>
2026-02-05 22:30:32 +0100

    footer

--- assets/js/client-slider.js
+++ assets/js/client-slider.js
@@ -9,18 +9,18 @@
     const prevEl = slider.querySelector(".client-logo-prev");
 
     new Swiper(slider, {
-      spaceBetween: 24,
+      spaceBetween: 16,
       loop: true,
       watchOverflow: true,
-      slidesPerView: 1,
-      slidesPerGroup: 1,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
       speed: 700,
       navigation: {
         nextEl,
         prevEl,
       },
       breakpoints: {
-        480: { slidesPerView: 2, slidesPerGroup: 2 },
+        480: { slidesPerView: 2, slidesPerGroup: 2, spaceBetween: 24 },
         768: { slidesPerView: 3, slidesPerGroup: 3 },
         1024: { slidesPerView: 4, slidesPerGroup: 4 },
         1280: { slidesPerView: 5, slidesPerGroup: 5 },
